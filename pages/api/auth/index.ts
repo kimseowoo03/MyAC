bca9baaa51908c812893599c6f672d783e48141f
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-20 14:22:05 +0900

    Fix: auth api라우터 error 수정

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -6,13 +6,20 @@ const apiHandler: NextApiHandler = async (
   req: ExtendedNextApiRequest,
   res: NextApiResponse
 ) => {
-  console.log("핸들러", req.token, req.user);
-  res.status(200).json({
-    _id: req.user._id,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-  });
+  try {
+    console.log("핸들러", req.token, req.user);
+    res.status(200).json({
+      _id: req.user._id,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "서버 오류가 발생했습니다.",
+      error: error,
+    });
+  }
 };
 
 export default authMiddleware(apiHandler);
