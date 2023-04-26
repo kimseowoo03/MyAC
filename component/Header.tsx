c7f571b9bf7dde43e3598429abc57b8b3b3f81d3
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-26 22:19:39 +0900

    Style: Header css->scss 수정

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import style from "../styles/Header.module.css";
+import style from "../styles/Header.module.scss";
 import { FiMenu, FiX } from "react-icons/fi";
 
 const Header = () => {
-  const [isToggled, setIsToggled] = useState(true);
+  const [isToggled, setIsToggled] = useState(false);
 
   const onClickMenuBtn = () => {
     setIsToggled(!isToggled);
@@ -17,14 +17,14 @@ const Header = () => {
     <header className={style.header}>
       <nav className={style.nav}>
         <div className={style.nav_loge}>
-          <a href="">MyAC</a>
+          <a href="">마이하근</a>
         </div>
         <ul className={navMenuStyle}>
           <li>
             <a>홈</a>
           </li>
           <li>
-            <a>학점계획표</a>
+            <a>학점이수계획표</a>
           </li>
           <li>
             <a>학점계산기</a>
